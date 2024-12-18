d7074c5c8c44c529da5df3721eec220542b5b83a
RCdeWit <rob.dewit@anyscale.com>
2024-12-18 14:51:19 +0100

    Create workflow to deploy basic service based on environment

---
+++ service/main.py
@@ -0,0 +1,19 @@
+import logging
+import os
+import requests
+from fastapi import FastAPI
+from ray import serve
+
+fastapi = FastAPI()
+logger = logging.getLogger("ray.serve")
+
+@serve.deployment
+@serve.ingress(fastapi)
+class FastAPIDeployment:
+    # FastAPI automatically parses the HTTP request.
+    @fastapi.get("/hello")
+    def say_hello(self, name: str) -> str:
+        logger.info("Handling request!")
+        return f"¡Hola {name}!"
+
+my_app = FastAPIDeployment.bind()
